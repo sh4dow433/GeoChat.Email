1603ad20b242fb3d14226ba25810fab0493b0356
David <merodavid30@yahoo.com>
2023-03-22 18:14:31 +0200

    Add project files.

---
+++ GeoChat.Email/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoChat.Email;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+    }
+}
